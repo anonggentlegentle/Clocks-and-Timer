6e57087f7ea2cea6462b959c32a6ed242dabc3d2
jarenkim <balberanjarenkim@gmail.com>
2022-10-01 18:35:50 +0800

    Add world clock

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 "use strict";
 
-const mainClock = document.querySelector(".clock__main-hours");
-const mainCurrentDay = document.querySelector(".clock__main-date");
+const mainClock = document.querySelector(".container__clock--main-hours");
+const mainCurrentDay = document.querySelector(".container__clock--main-date");
+const worldClocks = document.querySelector(".container__clock--world");
+
+mainClock.textContent = "";
+mainCurrentDay.textContent = "";
 
 const mainTimeAndDate = function () {
   const mainTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  // const mainOption = {
+  //   day: "numeric",
+  //   month: "long",
+  //   weekday: "long",
+  //   year: "numeric",
+  //   timeZone: "Asia/Tokyo",
+  // };
 
   const mainDate = new Intl.DateTimeFormat(navigator.language, {
     day: "numeric",
@@ -20,9 +32,50 @@ const mainTimeAndDate = function () {
       hour: "numeric",
       minute: "numeric",
       second: "numeric",
+      // timeZone: "Asia/Tokyo",
     }).format(new Date());
     mainClock.textContent = mainTime;
   }, 1000);
 };
 
 mainTimeAndDate();
+
+// console.log(Intl.supportedValuesOf("timeZone"));
+
+const countries = ["America/New_York", "Europe/London", "Asia/Tokyo"];
+
+countries.forEach((country, i) => {
+  const worldDate = document.createElement("div");
+
+  worldDate.className = `container__world--clock`;
+
+  const worldDay = new Intl.DateTimeFormat(navigator.language, {
+    day: "numeric",
+    month: "long",
+    weekday: "long",
+    year: "numeric",
+    timeZone: country,
+  }).format(new Date());
+
+  const firstPart = document.createElement("p");
+  firstPart.className = "container__clock--world-date";
+  firstPart.textContent = `${worldDay} - ${country.split("_").join(" ")}`;
+  worldDate.appendChild(firstPart);
+
+  const secondPart = document.createElement("p");
+  secondPart.className = "container__clock--world-hours";
+  secondPart.textContent = "";
+  worldDate.appendChild(secondPart);
+
+  setInterval(() => {
+    const worldTimeFormat = new Intl.DateTimeFormat(navigator.language, {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      timeZone: country,
+    }).format(new Date());
+    secondPart.textContent = worldTimeFormat;
+  }, 1000);
+
+  worldClocks.appendChild(worldDate);
+});
